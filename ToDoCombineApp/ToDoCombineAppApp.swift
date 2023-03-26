ff380070ae4b26f05ebbdb1e98be527a39769957
Kateryna Avramenko <kavramenko2000@gmail.com>
2023-03-26 12:56:50 +0300

    Initial Commit

---
+++ ToDoCombineApp/ToDoCombineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoCombineAppApp.swift
+//  ToDoCombineApp
+//
+//  Created by Kateryna Avramenko on 26.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoCombineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
